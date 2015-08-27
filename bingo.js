1a1a40dc985b813961d483ef25c7a011df1fb905
ivan.curkovic <ivan.curkovic@gmail.com>
2015-08-27 06:16:27 +0200

    refactor

--- bingo.js
+++ bingo.js
@@ -4,14 +4,12 @@ var _ = require('lodash');
 
 module.exports = function Bingo(){
   var self = this;
-  self.allNumbers = _.range(1,76);
+  self.avaliableNumbers = _.range(1,76);
 
   self.callNumber = function() {
-
-    var randomIndex = _.random(self.allNumbers.length - 1);
-    var removed = _.pullAt(self.allNumbers, randomIndex);
-    return removed[0];
-  }
+    var randomIndex = _.random(self.avaliableNumbers.length - 1);
+    return _.pullAt(self.avaliableNumbers, randomIndex)[0];
+  };
 
   return self;
 }
