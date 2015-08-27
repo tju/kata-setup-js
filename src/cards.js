f1ede6bd380b25db235320d1eb59e782868afdc2
ivan.curkovic <ivan.curkovic@gmail.com>
2015-08-27 07:12:39 +0200

    baby steps

--- src/cards.js
+++ src/cards.js
@@ -1,9 +1,18 @@
 
-module.exports = function Cards (){
+module.exports = function BingoCardsGenerator (){
   var self = this;
 
-  self.returnTrue = function (){
-    return true;
+  self.generate = function (){
+    var matrix = new Array(5);
+    matrix[0] = new Array(5);
+    matrix[1] = new Array(5);
+    matrix[2] = new Array(5);
+    matrix[3] = new Array(5);
+    matrix[4] = new Array(5);
+
+    matrix[0][0] = 12;
+
+    return matrix;
   }
 
   return self;
--- test/cardsSpec.js
+++ test/cardsSpec.js
@@ -1,12 +1,43 @@
 var expect = require('chai').expect;
 
-var Cards = require("../src/cards.js");
+var BingoCardsGenerator = require("../src/cards.js");
 
-var cards = new Cards();
+var bingoCardsGenerator;
 
 
-describe("Cards", function (){
-  it("should return true", function(){
-    expect(cards.returnTrue()).to.equals(true);
+describe("Cards", function () {
+  describe("Given I have a Bingo card generator", function () {
+    beforeEach(function () {
+      bingoCardsGenerator = new BingoCardsGenerator();
+    });
+
+    describe("When I generate a Bingo card", function () {
+      var card;
+
+      beforeEach(function () {
+        card = bingoCardsGenerator.generate();
+      });
+
+      it("Then the generated card has 25 unique spaces", function () {
+        expect(card.length).to.equal(5);
+        expect(card[0].length).to.equal(5);
+        expect(card[1].length).to.equal(5);
+        expect(card[2].length).to.equal(5);
+        expect(card[3].length).to.equal(5);
+        expect(card[4].length).to.equal(5);
+      });
+
+      it("And column B only contains numbers between 1 and 15 inclusive", function(){
+        expect(card[0][0]).to.be.most(15);
+        expect(card[0][0]).to.be.least(1);
+
+      });
+
+
+    });
+
+
   });
+
+
 });
