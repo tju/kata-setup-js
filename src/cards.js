63ae023c78ba999e345b722f58d1ef0dde979e73
ivan.curkovic <ivan.curkovic@gmail.com>
2015-08-27 07:53:55 +0200

    cards spec

--- src/cards.js
+++ src/cards.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 
 module.exports = function BingoCardsGenerator (){
   var self = this;
@@ -10,7 +12,18 @@ module.exports = function BingoCardsGenerator (){
     matrix[3] = new Array(5);
     matrix[4] = new Array(5);
 
-    matrix[0][0] = 12;
+    for(var i=0; i<5; i++){
+      matrix[0][i] = _.random(1, 15);
+      matrix[1][i] = _.random(16, 30);
+
+      if(i!==2){
+        matrix[2][i] = _.random(31, 45);
+      }
+
+      matrix[3][i] = _.random(46, 60);
+      matrix[4][i] = _.random(61, 75);
+    }
+
 
     return matrix;
   }
--- test/cardsSpec.js
+++ test/cardsSpec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-
+var _ = require('lodash');
 var BingoCardsGenerator = require("../src/cards.js");
 
 var bingoCardsGenerator;
@@ -28,11 +28,33 @@ describe("Cards", function () {
       });
 
       it("And column B only contains numbers between 1 and 15 inclusive", function(){
-        expect(card[0][0]).to.be.most(15);
-        expect(card[0][0]).to.be.least(1);
-
+        expect(_.min(card[0])).to.be.least(1);
+        expect(_.max(card[0])).to.be.most(15);
       });
 
+      it("And column I only contains numbers between 16 and 30 inclusive", function(){
+        expect(_.min(card[1])).to.be.least(16);
+        expect(_.max(card[1])).to.be.most(30);
+      });
+
+      it("And column N only contains numbers between 31 and 45 inclusive", function(){
+        expect(_.min(card[2])).to.be.least(31);
+        expect(_.max(card[2])).to.be.most(45);
+      });
+
+      it("And column G only contains numbers between 46 and 60 inclusive", function(){
+        expect(_.min(card[3])).to.be.least(46);
+        expect(_.max(card[3])).to.be.most(60);
+      });
+
+      it("And column O only contains numbers between 61 and 75 inclusive", function(){
+        expect(_.min(card[4])).to.be.least(61);
+        expect(_.max(card[4])).to.be.most(75);
+      });
+
+      it("And the generated card has 1 FREE space in the middle",function (){
+        expect(card[2][2]).to.be.empty;
+      })
 
     });
 
